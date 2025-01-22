5380a2d123028b8c6d3a11636a2ad8dc2d0fd0a2
Pepper Berry <pepperberry@outlook.com>
2025-01-21 16:41:25 -0800

    update to 5 servos

--- keyboard_controller.py
+++ keyboard_controller.py
@@ -3,7 +3,7 @@ import rclpy
 from rclpy.node import Node 
 
 #publish and subscribe to messages w/ floatng point numbers
-from std_msgs.msg import Float64 
+from std_msgs.msg import Float64MultiArray 
 
 import keyboard
 
@@ -11,21 +11,48 @@ class KeyboardController(Node):
     def __init__(self):
         super().__init__('keyboard_controller')
 #creates a publisher tp publish the messages w/ queue size 10
-        self.publisher_= self.create_publisher(Float64, 'servo_command', 10)
+        self.publisher_= self.create_publisher(Float64MultiArray, 'servo_commands', 10)
 #initalizes the servo angle to 90 degrees
-        self.angle = 90.0
+        self.angle = [90.0] * 5
 #cre3ates a timer that calls timer_callback every 0.1 seconds
         self.create_timer(0.1, self.timer_callback)
 #timer callback checks key and changes angle var then sends angle var message
+
+
     def timer_callback(self):
-        if keyboard.is_pressed('Q'):
-            self.angle += 1.0
-        elif keyboard.is_pressed('W'):
-            self.angle -= 1.0
-        msg = Float64()
-        msg.data = self.angle
+        #claw
+        if keyboard.is_pressed('1'):
+            self.angle[4] += 1.0
+        elif keyboard.is_pressed('2'):
+            self.angle[4] -= 1.0
+        #wrist
+        elif keyboard.is_pressed('q'):
+            self.angle[3] += 1.0
+        elif keyboard.is_pressed('w'):
+            self.angle[3] -= 1.0
+        #elbow
+        elif keyboard.is_pressed('a'):
+            self.angle[2] += 1.0
+        elif keyboard.is_pressed('s'):
+            self.angle[2] -= 1.0
+        #shoulder
+        elif keyboard.is_pressed('z'):
+            self.angle[1] += 1.0
+        elif keyboard.is_pressed('x'):
+            self.angle[1] -= 1.0
+        #base
+        elif keyboard.is_pressed('j'):
+            self.angle[0] += 1.0
+        elif keyboard.is_pressed('l'):
+            self.angle[0] -= 1.0
+    
+
+
+        #publishes message stuff
+        msg = Float64MultiArray()
+        msg.data = self.angles
         self.publisher_.publish(msg)
-        
+
 def main(args=None):
     rclpy.init(args=args)
     node = KeyboardController()
--- servo_controller.py
+++ servo_controller.py
@@ -15,17 +15,17 @@ class ServoController(Node):
     def __init__(self):
         super().__init__('servo_controller')
 #declaring parameters, should be set from launch files
-        self.declare_parameter(
+        self.declare_parameters(
             namespace='',
             parameters=[
-                ('servo_channel', [0, 1, 2, 3, 4]),
+                ('servo_channels', [0, 1, 2, 3, 4]),
                 ('frequency', 50),
                 ('min_pulse', 1000),
                 ('max_pulse', 2000)
             ]
         )
 #retrieves values and stores them
-        self.servo_channel = self.get_parameter('servo_channel').get_parameter_value().integer_array_value
+        self.servo_channel = self.get_parameter('servo_channels').get_parameter_value().integer_array_value
         self.frequency = self.get_parameter('frequency').get_parameter_value().integer_value
         self.min_pulse = self.get_parameter('min_pulse').get_parameter_value().integer_value
         self.max_pulse = self.get_parameter('max_pulse').get_parameter_value().integer_value
@@ -36,14 +36,14 @@ class ServoController(Node):
 #subsrcirbes to the servo command topic and calls listner_callback when message heard
         self.subscription = self.create_subscription(
             Float64MultiArray,
-            'servo_command',
+            'servo_commands',
             self.listner_callback,
             10)
         self.subscription
 #defines the listner callback method which process incoming messahes converting an angle into a pulse wicth ect...
     def listener_callback(self, msg):
         for i, angle in enumerate(msg.data):
-            if i < len(self.servo_channels):
+                if i < len(self.servo_channels):
                         pulse_width = self.min_pulse + (self.max_pulse - self.min_pulse) * (angle /180.0)
                         pulse_length = int(pulse_width / 1000000 * self.frequency * 4096)
                         self.pwm.channels[self.servo_channels[i]].duty_cycle = pulse_length
