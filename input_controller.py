72f1b363724b44f497860974bc5f0d5f190ce856
Pepper Berry <51861992+pepperberry@users.noreply.github.com>
2025-01-29 15:29:54 -0800

    Update input_controller.py

--- input_controller.py
+++ input_controller.py
@@ -1,21 +1,35 @@
-#this is our publisher file
+"""
+author: Pepper Berry
+date: jan 29th 2025
+use: this is our publisher file for the simplest motor movement possible in ros2 
+with actual inputs!!! comments in this file will only be about the changes between this file and input_easy.py
+use in conjunction with the servo_single.py our subsriber file
+"""
 
-#ros 2 python client library
 import rclpy
 from rclpy.node import Node
 
-#publish and subscribe to messages w/ floatng point numbers (degrees of rotation)
 from std_msgs.msg import Float64
 
-#initilzing
 class InputControl(Node):
   def __init__(self):
     super().__init__('input_controller')
+    
+    #setup an inital angle to modify 
     self.angle = 90.0
+    
     self.publisher_=self.create_publisher(Float64, 'servo_command', 10)
+    self.first();
     self.run();
 
-  #the code for entering commands
+  #passed a intial message to set the motor to our intial angle
+  def first(self):
+    msg = Float64MultiArray()
+    msg.data = self.angle
+    self.publisher.publish(msg)
+    print("published inital")
+
+   #input how you want it to move changes the inital angle then publishes it
   def run(self):
     try:
       while rclpy.ok():
