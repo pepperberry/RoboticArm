d2967683bc0d95f52e38277c59e5c63e26bdf9de
Pepper Berry <51861992+pepperberry@users.noreply.github.com>
2025-01-28 22:19:12 -0800

    Update servo_controller.py

--- servo_controller.py
+++ servo_controller.py
@@ -15,7 +15,7 @@ class ServoController(Node):
     def __init__(self):
         super().__init__('servo_controller')
         self.set_servo_angle(0, 90.0)
-        self.subscription = self.create_subscription(Float64, 'angle_command', self.listner_callback, 10)
+        self.subscription = self.create_subscription(Float64, 'servo_command', self.listner_callback, 10)
         self.subscription
         self.pca = adafruit_pca9685.PCA9685(board.I2C())
         self.pca.frequency = 50
