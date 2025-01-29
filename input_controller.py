5c90cfe34a595e9cf00f3ab698ea8aedf2ccb006
Pepper Berry <51861992+pepperberry@users.noreply.github.com>
2025-01-28 17:05:35 -0800

    Create input_controller.py

---
+++ input_controller.py
@@ -0,0 +1,35 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Float64MultiArray
+
+class InputControl(Node):
+  def __init__(self):
+    super().__init__('input_controller')
+    self.angle = [90.0] * 5
+    self.publisher_=self.create_publisher(Float64MultiArray, 'servo_commands', 10)
+    self.run();
+  def run(self):
+    try:
+      while rclpy.ok():
+        print("captuerd command")
+        if command == 'a':
+          self.angle[0] += 1.0
+        elif command == 's':
+          self.angle[0] -=1.0
+        msg = Float64MultiArray()
+        msg.data = self.angles
+        self.publisher_.publish(msg)
+        print("published")
+    except KeyboardInterruipt:
+      pass
+
+def main(args=None):
+  rclpy.init(args=args)
+  input_control = InputControl()
+  rclpy.shutdown()
+
+if __name__ == '__main__':
+  main()
+      
+
