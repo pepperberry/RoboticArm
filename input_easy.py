59eb9656cbf88d1ed6fad3a036ecd6deaf4753da
Pepper Berry <51861992+pepperberry@users.noreply.github.com>
2025-01-29 15:20:08 -0800

    Update input_easy.py

--- input_easy.py
+++ input_easy.py
@@ -1,14 +1,27 @@
+"""
+author: Pepper Berry
+date: jan 29th 2025
+use: this is our publisher file for the simplest motor movement possible in ros2
+use in conjunction with the servo_single.py our subsriber file
+"""
+
+#ros 2 python client library
 import rclpy
 from rclpy.node import Node
 
+#publish and subscribe to messages w/ floatng point numbers (degrees of rotation)
 from std_msgs.msg import Float64
 
+#initalizing
 class InputEasy(Node):
   def __init__(self):
     super().__init__('input_easy')
+
+    #creating a publisher, publishing the topic 'servo_command' this is what links our subscriber and publisher files
     self.publisher_=self.create_publisher(Float64, 'servo_command', 10)
     self.run();
-    
+
+  #basically just publishes 40 degrees to the topic change if needed change this
   def run(self):
     msg = Float64()
     msg.data = 40.0
