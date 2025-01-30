722c53ac1adf9157fa33776dd84c56238499a9a1
Pepper Berry <51861992+pepperberry@users.noreply.github.com>
2025-01-30 15:01:50 -0800

    Update and rename keyboard_input.py to pub_multi_input.py

--- pub_multi_input.py
+++ pub_multi_input.py
@@ -11,12 +11,12 @@ from rclpy.node import Node
 
 from std_msgs.msg import Float64MultiArray
 
-class InputControl(Node):
+class PubMultiInput(Node):
   def __init__(self):
-    super().__init__('input_controller')
+    super().__init__('pub_multi_input')
     
     #setup an inital angle to modify 
-    self.angle = [245] 
+    self.angle = [245,0,0,0,0] 
     
     self.publisher_=self.create_publisher(Float64MultiArray, 'servo_command', 10)
     self.first();
@@ -35,10 +35,26 @@ class InputControl(Node):
       while rclpy.ok():
         command = input("enter command: ")
         print("captuerd command")
-        if command == 'a':
-          self.angle += 1.0
+        if command == 'z':
+          self.angle[0] += 1.0
+        elif command == 'x':
+          self.angle[0] -=1.0
+        elif command == 'a':
+          self.angle[1] += 1.0
         elif command == 's':
-          self.angle -=1.0
+          self.angle[1] -=1.0
+        elif command == 'q':
+          self.angle[2] += 1.0
+        elif command == 'w':
+          self.angle[2] -=1.0
+        elif command == '1':
+          self.angle[3] += 1.0
+        elif command == '2':
+          self.angle[3] -=1.0
+        elif command == 'e':
+          self.angle[4] += 1.0
+        elif command == 'r':
+          self.angle[4] -=1.0
         msg = Float64MultiArray()
         msg.data = self.angle
         self.publisher_.publish(msg)
@@ -48,7 +64,7 @@ class InputControl(Node):
 
 def main(args=None):
   rclpy.init(args=args)
-  input_control = InputControl()
+  input_control = PubMultiInput()
   rclpy.shutdown()
 
 if __name__ == '__main__':
